83bc62f876481663b31694d7ecbf12f2dc9cd556
Bkha6000 <55024814+Bkha6000@users.noreply.github.com>
2021-01-18 19:07:57 -0500

    Update MarkType.cs

--- MarkType.cs
+++ MarkType.cs
@@ -4,7 +4,7 @@ namespace TicTacToe
 {
     /// <summary>
     /// Enum that represents the different markings that may be on a button for tic-tac-toe.
-    /// They are cross, empty and circle. 
+    /// They are cross, free, and circle. 
     /// </summary>
     public enum MarkType
     {
